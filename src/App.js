78c4c125e72d9ab467ce05c0b2c01d9024911fbd
Aimarall <aimaral21072004@gmail.com>
2021-04-01 14:36:03 +0600

    added email and title to Example1 and to Example2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { useState } from 'react';
 import './App.css';
 import FullPost from './components/FullPost/FullPost';
 import Posts from './components/Posts/Posts';
+import Example1 from './components/Example1/Example1';
+import Example2 from './components/Example2/Example2';
 
 function App() {
   const [selectedPost, setSelectedPost] = useState(0);
 
   return (
     <div className="App">
-      <FullPost id={selectedPost} />
-      <Posts setSelectedPost={setSelectedPost} />
+      <Example1 />
+      <Example2 />
+      {/* <FullPost id={selectedPost} />
+      <Posts setSelectedPost={setSelectedPost} /> */}
     </div>
   );
 }
